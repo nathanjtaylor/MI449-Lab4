ebfa627ff4b39e087d8c3d6ddad6f5e87de6f17c
Nathan <LilFuze9@gmail.com>
2025-02-05 11:48:43 -0500

    content

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode(
+    "card",
+    (title, subtitle) => {
+      return `<div class="card ${title}"><div class="icon ${title}"></div><strong>${title}</strong>${subtitle}</div>`;
+    }
+  );
 };
